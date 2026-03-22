cba1d8fbe2d38816501c05349d134aef1e05c9e6
mavyfaby <maverickfabroa@gmail.com>
2026-03-23 01:00:35 +0800

    feat(app): add .env loader from dynamic env file

---
+++ src/config.rs
@@ -0,0 +1,45 @@
+use std::env;
+
+pub struct AppConfig {
+    pub tz: String,
+
+    pub postgres_host: String,
+    pub postgres_port: u16,
+    pub postgres_user: String,
+    pub postgres_password: String,
+    pub postgres_db: String,
+}
+
+impl AppConfig {
+    pub fn load() -> Self {
+        let env_file = env::var("ENV_FILE")
+            .expect("ENV_FILE env is required.");
+
+        let env_file_path_raw = env::current_dir()
+            .expect("Error occurred while getting current directory.")
+            .join(env_file);
+
+        let env_file_path = env_file_path_raw.to_str()
+            .expect("Error occurred while converting the env file to string");
+
+        dotenvy::from_path(env_file_path)
+            .expect("Cannot load environment file.");
+
+        Self {
+            tz: env::var("TZ")
+                .expect("TZ env is required."),
+            postgres_host: env::var("POSTGRES_HOST")
+                .expect("POSTGRES_HOST env is required."),
+            postgres_port: env::var("POSTGRES_PORT")
+                .expect("POSTGRES_PORT env is required.")
+                .parse::<u16>()
+                .expect("POSTGRES_PORT must be a valid port."),
+            postgres_user: env::var("POSTGRES_USER")
+                .expect("POSTGRES_USER env is required."),
+            postgres_password: env::var("POSTGRES_PASSWORD")
+                .expect("POSTGRES_PASSWORD env is required."),
+            postgres_db: env::var("POSTGRES_DB")
+                .expect("POSTGRES_DB env is required."),
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+mod config;
+
+use config::AppConfig;
+
 fn main() {
-    println!("Hello, world!");
+    let config = AppConfig::load();
+
+    println!("TZ: {}", config.tz);
+    println!("POSTGRES_HOST: {}", config.postgres_host);
+    println!("POSTGRES_PORT: {}", config.postgres_port);
+    println!("POSTGRES_USER: {}", config.postgres_user);
+    println!("POSTGRES_PASSWORD: {}", config.postgres_password);
+    println!("POSTGRES_DB: {}", config.postgres_db);
 }
